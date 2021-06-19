ffacdc4d6f765b17e2b2977e7e25ca91214dccd8
chris <chrissantosproduction@gmail.com>
2021-06-19 10:05:07 -0500

    Couple fixes

--- src/pages/about/about-page.tsx
+++ src/pages/about/about-page.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Typer from '../../components/atoms/typer';
-import img from '../../images/star_wars.jpeg';
+import Section, { SectionAnimations } from '../../components/molecules/section/section';
+import swImg from '../../images/star_wars.jpeg';
+import crossfitImg from '../../images/crossfit.jpg';
 import './about-page.css';
 import Page from '../page';
 
 const AboutPage = () => {
     return (
         <Page>
-            <section>
+            <Section animate animation={SectionAnimations.SlideInLeft}>
                 <div className="card">
                     <div className="card-header">
-                        <h1>About Me</h1>
-                        <small className="aurebesh">About Me</small>
+                        <h1>Star Wars</h1>
+                        <small className="aurebesh">Star Wars</small>
                     </div>
                     <div>
                         <Typer lines={[
@@ -22,13 +24,34 @@ const AboutPage = () => {
                         ]} />
                         <figure className="flex flex-column align-items-center">
                             <div>
-                                <img className="star-wars-image" alt="Star Wars" src={img}/>
+                                <img className="star-wars-image" alt="Star Wars" src={swImg}/>
                             </div>
                             <figcaption>STAR WARS - EPISODE VIII - IT IS TIME FOR THE JEDI TO END - VARIANT - Dan Mumford</figcaption>
                         </figure>
                     </div>
                 </div>
-            </section>
+            </Section>
+            <Section animate animation={SectionAnimations.SlideInRight}>
+                <div className="card">
+                    <div className="card-header">
+                        <h1>CrossFit</h1>
+                        <small className="aurebesh">crossfit</small>
+                    </div>
+                    <div>
+                        <Typer lines={[
+                            `I also like to work out. I started CrossFit in 2016 and have been doing it ever since. I never though that I would be able
+                            to do half of the things I am now capable of. CrossFit has taught me that I can continue to push no matter what, and that the 
+                            impossible is achievable, so long as you put the effort in. Here's a picture of my box family on Memorial Day of 2021.`
+                        ]} />
+                        <figure className="flex flex-column align-items-center">
+                            <div>
+                                <img className="star-wars-image" alt="Star Wars" src={crossfitImg}/>
+                            </div>
+                            <figcaption>MURPH - Memorial Day 2021</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </Section>
         </Page>
     )
 }
--- src/pages/gallery/gallery-page.tsx
+++ src/pages/gallery/gallery-page.tsx
@@ -2,24 +2,32 @@ import React from 'react';
 import './gallery-page.css';
 import Page from '../page';
 import Section, { SectionAnimations } from '../../components/molecules/section/section';
-import img from '../../images/star_wars.jpeg';
+import fam from '../../images/fam.jpg';
+import bu from '../../images/bu.jpg';
+import engagement from '../../images/engagement.jpg';
+import juan from '../../images/juan.jpg';
 
 const GalleryPage = () => {
     return (
         <Page>
+            <Section animate animation={SectionAnimations.SlideInRight}>
+                <div className="flex justify-content-center">
+                    <img alt="Engagement" className="gallery-picture" src={engagement} />
+                </div>
+            </Section>
             <Section animate animation={SectionAnimations.SlideInLeft}>
                 <div className="flex justify-content-center">
-                    <img src={img} />
+                    <img alt="Family" className="gallery-picture" src={fam} />
                 </div>
             </Section>
             <Section animate animation={SectionAnimations.SlideInRight}>
                 <div className="flex justify-content-center">
-                    <img src={img} />
+                    <img alt="BU Graduation" className="gallery-picture" src={bu} /> 
                 </div>
             </Section>
             <Section animate animation={SectionAnimations.SlideInLeft}>
                 <div className="flex justify-content-center">
-                    <img src={img} /> 
+                    <img alt="My Friend Juan" className="gallery-picture" src={juan} /> 
                 </div>
             </Section>
         </Page>
